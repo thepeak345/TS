ff8a25321ebd86760c6dc809e21da266e31d8c67
nastyashandr <nshandr368@gmail.com>
2023-05-13 18:27:44 +0300

    pairs

--- game/utils.py
+++ game/utils.py
@@ -3,11 +3,12 @@ from game.models import Pair
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+
 def box_pair(box):
     qs = list(User.objects.filter(box=box).values_list('id', flat=True))
     random.shuffle(qs)
     pairs = list(zip(qs[1::2], qs[::2]))
     for pair in pairs:
-        player1, player2 = pair
+        player1, player2 = User.objects.get(pk=pair[0]), User.objects.get(pk=pair[1])
         Pair.objects.create(box=box, player1=player1, player2=player2)
-
